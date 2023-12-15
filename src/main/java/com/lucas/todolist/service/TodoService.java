fd1230b431fec04a541f7ea48fec3dcda3c5611e
Lucas Morais <lucas14.morais@gmail.com>
2023-12-15 13:25:20 -0300

    feat(service): add list method in TodoService

--- src/main/java/com/lucas/todolist/service/TodoService.java
+++ src/main/java/com/lucas/todolist/service/TodoService.java
@@ -1,7 +1,11 @@
 package com.lucas.todolist.service;
 
+import com.lucas.todolist.entity.Todo;
 import com.lucas.todolist.repository.TodoRepository;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class TodoService {
@@ -9,5 +13,12 @@ public class TodoService {
 
     public TodoService(TodoRepository todoRepository) {
         this.todoRepository = todoRepository;
+    }
+
+    public List<Todo> list() {
+        Sort sort = Sort.by("priority").descending().and(
+                Sort.by("name").ascending());
+
+        return todoRepository.findAll(sort);
     }
 }
