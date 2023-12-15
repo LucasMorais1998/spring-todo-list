4d4d3bcb4a6c7cecb1111b6706abde976b5e1b2a
Lucas Morais <lucas14.morais@gmail.com>
2023-12-15 13:39:58 -0300

    feat(controller): add create method in TodoController

--- src/main/java/com/lucas/todolist/controller/TodoController.java
+++ src/main/java/com/lucas/todolist/controller/TodoController.java
@@ -1,9 +1,22 @@
 package com.lucas.todolist.controller;
 
+import com.lucas.todolist.entity.Todo;
+import com.lucas.todolist.service.TodoService;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/todos")
 public class TodoController {
+    private TodoService todoService;
+
+    public TodoController(TodoService todoService) {
+        this.todoService = todoService;
+    }
+
+    public List<Todo> create(Todo todo) {
+        return todoService.create(todo);
+    }
 }
