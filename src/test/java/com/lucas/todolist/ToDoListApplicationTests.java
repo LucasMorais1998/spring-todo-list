566b7e2b4ca36b8ce80ff61770fbfb013973266b
Lucas Morais <lucas14.morais@gmail.com>
2023-12-16 17:50:19 -0300

    test(e2e): add testCreateTodoFailure

--- src/test/java/com/lucas/todolist/ToDoListApplicationTests.java
+++ src/test/java/com/lucas/todolist/ToDoListApplicationTests.java
@@ -31,4 +31,16 @@ class ToDoListApplicationTests {
 
     }
 
+    @Test
+    void testCreateTodoFailure() {
+        webTestClient
+                .post()
+                .uri("/todos")
+                .bodyValue(
+                        new Todo("", "", false, 0)
+                )
+                .exchange()
+                .expectStatus().isBadRequest();
+    }
+
 }
