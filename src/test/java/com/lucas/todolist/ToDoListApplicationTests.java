9fc8189acdce5a5eb9a195137b2a0e0da58fdc59
Lucas Morais <lucas14.morais@gmail.com>
2023-12-16 18:12:09 -0300

    test(e2e): add testGetAllTodosSuccess

--- src/test/java/com/lucas/todolist/ToDoListApplicationTests.java
+++ src/test/java/com/lucas/todolist/ToDoListApplicationTests.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
+import static org.springframework.http.MediaType.APPLICATION_JSON;
+
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class ToDoListApplicationTests {
     @Autowired
@@ -28,7 +30,6 @@ class ToDoListApplicationTests {
                 .jsonPath("$[0].description").isEqualTo(todo.getDescription())
                 .jsonPath("$[0].complete").isEqualTo(todo.isComplete())
                 .jsonPath("$[0].priority").isEqualTo(todo.getPriority());
-
     }
 
     @Test
@@ -43,4 +44,14 @@ class ToDoListApplicationTests {
                 .expectStatus().isBadRequest();
     }
 
+    @Test
+    void testGetAllTodosSuccess() {
+        webTestClient
+                .get()
+                .uri("/todos")
+                .exchange()
+                .expectStatus().isOk()
+                .expectHeader().contentType(APPLICATION_JSON)
+                .expectBodyList(Todo.class);
+    }
 }
